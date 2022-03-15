855624fd7144587a5a5d161b5d74771dc9ffd494
Joy Shi <ac106780@avcol.school.nz>
2022-03-16 12:25:06 +1300

    One page scrolling + favicon added

--- WebApplication1/wwwroot/css/js/site.js
+++ WebApplication1/wwwroot/css/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$('a[href*="#"]:not([href="#"]):not([href="#show"]):not([href="#hide"])').click(function () {
+	if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+		var target = $(this.hash);
+		target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+		if (target.length) {
+			$('html,body').animate({
+				scrollTop: target.offset().top
+			}, 1000);
+			return false;
+		}
+	}
+});
